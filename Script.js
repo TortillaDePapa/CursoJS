d2bb33c45dcb2c3621307ba547066a6a69a862b1
TortillaDePapa <benjamin25654@gmail.com>
2025-05-01 20:52:38 -0300

    NuevoCommit

--- Script.js
+++ Script.js
@@ -2,3 +2,17 @@ let Nombre = 'Benja';
 
 
 console.log(Nombre);
+
+let iteracion = 10;
+let numero = 5;
+
+
+function sumaIterada(numero , iteracion){
+        for(let i=0; i<iteracion;i++){
+            numero += numero
+        }
+
+        console.log(numero);
+}
+
+sumaIterada(numero,iteracion)
